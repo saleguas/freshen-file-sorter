4f2f8f85c0e02c89cedfb42de49802f984b2a336
salvador aleguas <salvadoraleguas@gmail.com>
2019-12-29 03:26:55 -0500

    Release 1.3.2

--- build.bat
+++ build.bat
@@ -5,5 +5,5 @@ ROBOCOPY "src/scripts/" "dist" *.py
 mkdir "dist/config"
 ROBOCOPY "src/config/" "dist/config" *.yml
 7z a -r dist.zip dist/
-release-it
+release-it --no-git.requireCleanWorkingDir
 pause
